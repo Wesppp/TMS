ad882caaca63185ca8ce6165fffe9dc9bcdeae56
Wesppp <mshesnovich@mail.ru>
2024-01-04 13:57:00 +0300

    add switching styles in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgOptimizedImage } from '@angular/common';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
+import { NgClass, NgOptimizedImage, NgStyle } from '@angular/common';
+
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { NavigationLink } from '../../models/nav-link.interface';
 import { navLinks } from '../../constants/nav-links';
@@ -12,10 +14,27 @@ import { navLinks } from '../../constants/nav-links';
     RouterLink,
     RouterLinkActive,
     NgOptimizedImage,
+    NgClass,
+    NgStyle
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   public navLinks: NavigationLink[] = navLinks;
+  public isHomePage: boolean = false;
+
+  constructor(private readonly router: Router,
+              private readonly destroyRef: DestroyRef) {
+  }
+
+  public ngOnInit(): void {
+    this.router.events.pipe(
+     takeUntilDestroyed(this.destroyRef)
+    ).subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isHomePage = this.router.url.includes('home');
+      }
+    });
+  }
 }
--- src/app/constants/nav-links.ts
+++ src/app/constants/nav-links.ts
@@ -1,7 +1,7 @@
 import { NavigationLink } from '../models/nav-link.interface';
 
 export const navLinks: NavigationLink[] = [
-  { label: 'Home', link: '#' },
+  { label: 'Home', link: '/home' },
   { label: 'Shop', link: '#' },
   { label: 'Account', link: '#' },
   { label: 'Pages', link: '#' },
