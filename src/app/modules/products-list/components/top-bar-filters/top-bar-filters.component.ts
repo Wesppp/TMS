ce7a397ce2ebf4d7653f1cbe52156c5100ad5f26
wesppp <mshesnovich@mail.ru>
2024-01-22 12:57:25 +0300

    create mixin for maxWidthContainer

---
+++ src/app/modules/products-list/components/top-bar-filters/top-bar-filters.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar-filters',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-bar-filters.component.html',
+  styleUrl: './top-bar-filters.component.scss'
+})
+export class TopBarFiltersComponent {
+
+}
--- src/app/modules/products-list/products-list.component.ts
+++ src/app/modules/products-list/products-list.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { PageTitleComponent } from "@components/page-title/page-title.component";
 import { SideBarFiltersComponent } from "@modules/products-list/components/side-bar-filters/side-bar-filters.component";
+import { TopBarFiltersComponent } from "@modules/products-list/components/top-bar-filters/top-bar-filters.component";
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
   imports: [
     PageTitleComponent,
-    SideBarFiltersComponent
+    SideBarFiltersComponent,
+    TopBarFiltersComponent
   ],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.scss'
