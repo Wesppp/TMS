37f759f191caaada44c2e6cc11a2979dbc469fa3
wesppp <mshesnovich@mail.ru>
2024-01-29 14:32:09 +0300

    started creating product-info template

--- src/app/modules/product-details/product-details.component.ts
+++ src/app/modules/product-details/product-details.component.ts
@@ -1,14 +1,70 @@
-import { Component, Input } from '@angular/core';
+import { Component, DestroyRef, Input, OnInit } from '@angular/core';
+import { AsyncPipe } from "@angular/common";
+
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { GalleriaModule } from "primeng/galleria";
+
+import { getProductAction } from "@store/products/actions/get-product.action";
+import { Product } from "@models/product.interface";
+import { productSelector } from "@store/products/products.selectors";
+import { isProductLoadingSelector } from "@store/app-loading/app-loading.selectors";
+import { ProgressSpinnerComponent } from "@components/progress-spinner/progress-spinner.component";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { RatingModule } from "primeng/rating";
+import { SizeColorPickerComponent } from "@components/size-color-picker/size-color-picker.component";
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    ProgressSpinnerComponent,
+    GalleriaModule,
+    RatingModule,
+    SizeColorPickerComponent,
+  ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
-export class ProductDetailsComponent {
-  @Input() private set id(productId: string) {
-    console.log(productId)
+export class ProductDetailsComponent implements OnInit {
+  @Input() private set id(id: string) {
+    this.store.dispatch(getProductAction({ id }));
+  }
+
+  public isProductLoading$!: Observable<boolean>;
+
+  public product!: Product | null;
+  public galleryOptions: any[] = [
+    {
+      breakpoint: '1024px',
+      numVisible: 5
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 3
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1
+    }
+  ];
+
+  constructor(private readonly store: Store,
+              private readonly destroyRef: DestroyRef) {
+  }
+
+  public ngOnInit(): void {
+    this.initializeValues();
+  }
+
+  private initializeValues(): void {
+    this.store.select(productSelector)
+      .pipe(
+        takeUntilDestroyed(this.destroyRef)
+      ).subscribe(product => {
+        this.product = product;
+    });
+    this.isProductLoading$ = this.store.select(isProductLoadingSelector);
   }
 }
