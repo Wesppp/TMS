396e057815752e7664f4b793f211954dd91f3dba
wesppp <mshesnovich@mail.ru>
2024-01-30 13:23:18 +0300

    fix issue with header fields

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AsyncPipe, NgClass, NgOptimizedImage, NgStyle, NgTemplateOutlet } from '@angular/common';
 
-import { Store } from '@ngrx/store';
-import { Observable, of } from 'rxjs';
-
 import { NavigationLink } from '@models/nav-link.interface';
 import { NAV_LINKS } from '@constants/nav-links';
-import { isLoggedInSelector } from '@store/auth/auth.selectors';
 import { AccountActions } from '@models/account-actions.interface';
 import { ACCOUNT_ACTIONS } from '@constants/account-actions';
-import { isSecondHeaderSelector } from "@store/header/header.selectors";
 
 @Component({
   selector: 'app-header',
@@ -27,22 +22,10 @@ import { isSecondHeaderSelector } from "@store/header/header.selectors";
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements OnInit {
-  public isLoggedIn$: Observable<boolean> = of(false);
-  public isSecondHeader$: Observable<boolean> = of(false);
+export class HeaderComponent {
+  @Input({ required: true }) public isLoggedIn: boolean | null = false;
+  @Input() public isSecondHeader: boolean | null = false;
 
   public navLinks: NavigationLink[] = NAV_LINKS;
   public accountActions: AccountActions[] = ACCOUNT_ACTIONS;
-
-  constructor(private readonly store: Store) {
-  }
-
-  public ngOnInit(): void {
-    this.initializeValues();
-  }
-
-  private initializeValues(): void {
-    this.isLoggedIn$ = this.store.select(isLoggedInSelector);
-    this.isSecondHeader$ = this.store.select(isSecondHeaderSelector);
-  }
 }
--- src/app/modules/store/store.component.ts
+++ src/app/modules/store/store.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
-import {FooterComponent} from "@components/footer/footer.component";
-import {HeaderComponent} from "@components/header/header.component";
-import {RouterOutlet} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+import { Observable } from "rxjs";
+import { Store } from "@ngrx/store";
+
+import { FooterComponent } from "@components/footer/footer.component";
+import { HeaderComponent } from "@components/header/header.component";
+import { isLoggedInSelector } from "@store/auth/auth.selectors";
+import { isSecondHeaderSelector } from "@store/header/header.selectors";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
   selector: 'app-store',
   standalone: true,
-    imports: [
-        FooterComponent,
-        HeaderComponent,
-        RouterOutlet
-    ],
+  imports: [
+    FooterComponent,
+    HeaderComponent,
+    RouterOutlet,
+    AsyncPipe
+  ],
   templateUrl: './store.component.html',
   styleUrl: './store.component.scss'
 })
-export class StoreComponent {
+export class StoreComponent implements OnInit {
+  public isLoggedIn$!: Observable<boolean>;
+  public isSecondHeader$!: Observable<boolean>;
 
+  constructor(private readonly store: Store) {
+  }
+
+  public ngOnInit(): void {
+    this.initializeValues();
+  }
+
+  private initializeValues(): void {
+    this.isLoggedIn$ = this.store.select(isLoggedInSelector);
+    this.isSecondHeader$ = this.store.select(isSecondHeaderSelector);
+  }
 }
