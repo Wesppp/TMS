27e798d402330bd884fb1b2b0fc8c52bd36b82e0
wesppp <mshesnovich@mail.ru>
2024-02-02 14:48:08 +0300

    fix login auth loading

--- src/store/auth/effects/auth-loadings.effect.ts
+++ src/store/auth/effects/auth-loadings.effect.ts
@@ -4,11 +4,10 @@ import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { tap } from "rxjs";
 import { Store } from "@ngrx/store";
 
-import { logoutAction } from "@store/auth/actions/logout.action";
 import { registerAction, registerFailureAction, registerSuccessAction } from "@store/auth/actions/register.action";
 import { addLoadingAction, removeLoadingAction } from "@store/app-loading/actions/loading-crud.action";
 import { AppLoadings } from "@enums/app-loading.enum";
-import { loginFailureAction, loginSuccessAction } from "@store/auth/actions/login.action";
+import { loginAction, loginFailureAction, loginSuccessAction } from "@store/auth/actions/login.action";
 
 @Injectable()
 export class AuthLoadingsEffect {
@@ -18,7 +17,7 @@ export class AuthLoadingsEffect {
 
   addAuthLoading$ = createEffect(
     () => this.actions$.pipe(
-      ofType(logoutAction, registerAction),
+      ofType(loginAction, registerAction),
       tap(() => {
         this.store.dispatch(addLoadingAction( { loadingName: AppLoadings.AUTH_LOADING } ));
       })
