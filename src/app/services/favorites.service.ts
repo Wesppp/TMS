8c7332882dc2209e5e4e797b1563a28d351b4964
wesppp <mshesnovich@mail.ru>
2024-02-09 17:10:26 +0300

    create localstorage service

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -6,43 +6,44 @@ import { MessageService } from "primeng/api";
 import { LocalStorageKeys } from "@enums/localstorage-keys.enum";
 import { Product } from "@models/product.interface";
 import { ProductsService } from "@services/products.service";
+import { LocalStorageService } from "@services/local-storage.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
   constructor(private readonly messageService: MessageService,
-              private readonly productsService: ProductsService) {
+              private readonly productsService: ProductsService,
+              private readonly localStorageService: LocalStorageService) {
   }
 
   public addToFavorite(productUuid: string): Observable<string> {
-    const existingFavorites: string | null = localStorage.getItem(LocalStorageKeys.FAVORITES);
+    const existingFavorites: string[] | null = this.localStorageService.getStorageData(LocalStorageKeys.FAVORITES);
 
-    if(existingFavorites && JSON.parse(existingFavorites).includes(productUuid)) {
+    if(existingFavorites && existingFavorites.includes(productUuid)) {
       this.messageService.add({ severity: 'warn', summary: 'Warn', detail: 'You have already added the product to your favorites.' });
 
       return of();
     }
 
-    const updateFavorite: string[] = existingFavorites ? [...JSON.parse(existingFavorites), productUuid] : [productUuid];
-
-    localStorage.setItem(LocalStorageKeys.FAVORITES, JSON.stringify(updateFavorite));
-
-    return of(productUuid);
+    return of(
+      this.localStorageService.addElementToStorage(LocalStorageKeys.FAVORITES, productUuid)
+    );
   }
 
   public removeFromFavorite(productUuid: string): Observable<string> {
-    const favoriteProducts: string[] = JSON.parse(localStorage.getItem(LocalStorageKeys.FAVORITES)!);
-    const updatedFavorites: string[] = favoriteProducts.filter((uuid) => uuid === productUuid);
+    const favoriteProducts: string[] = this.localStorageService.getStorageData(LocalStorageKeys.FAVORITES)!;
+    const updatedFavorites: string[] = favoriteProducts.filter((uuid) => uuid !== productUuid);
 
-    localStorage.setItem(LocalStorageKeys.FAVORITES, JSON.stringify(updatedFavorites));
+    this.localStorageService.updateStorageElements(LocalStorageKeys.FAVORITES, updatedFavorites);
 
     return of(productUuid);
   }
 
   public getFavoriteProducts(): Observable<Product[]> {
-    const existingCart: string | null = localStorage.getItem(LocalStorageKeys.FAVORITES);
-    const favoriteProducts: string[] = existingCart ? JSON.parse(existingCart) : [];
+    const favoriteProducts: string[] | null = this.localStorageService.getStorageData(LocalStorageKeys.FAVORITES);
+
+    if(!favoriteProducts) { return of([]); }
 
     return this.productsService.getProducts({}).pipe(
       map((products) => favoriteProducts.map((uuid: string) =>
